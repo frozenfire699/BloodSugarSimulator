e91697ab0daf6643e5426641f146e4f3cf55253f
Frozenfire <Frozenfire@megro.domain>
2017-06-20 17:48:24 -0600

    Initial checkin of the project

---
+++ src/org/rohit/test/bloodsugar/simulator/SimulatorClient.java
@@ -0,0 +1,12 @@
+package org.rohit.test.bloodsugar.simulator;
+
+/**
+ * This is a client class that will call the Simulator engine
+ * This will pass the inputs and will make result in getting the graph drawn
+ * @author Frozenfire
+ *
+ */
+public class SimulatorClient {
+
+
+}
---
+++ src/org/rohit/test/bloodsugar/simulator/SimulatorEngine.java
@@ -0,0 +1,12 @@
+package org.rohit.test.bloodsugar.simulator;
+
+/**
+ * This class will act the simulator enigne and will be responsible for providing
+ * the points that will be plotted on the graph
+ * 
+ * @author Frozenfire
+ *
+ */
+public class SimulatorEngine {
+
+}
